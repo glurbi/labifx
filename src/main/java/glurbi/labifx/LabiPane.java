f0da40a0e4a83eb32d9df658f16c6f0373c18b0c
Vincent Gay <glurbi@gmail.com>
2012-12-27 20:58:08 +0100

    use path instead of many lines

--- src/main/java/glurbi/labifx/LabiPane.java
+++ src/main/java/glurbi/labifx/LabiPane.java
@@ -2,15 +2,17 @@ package glurbi.labifx;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.scene.Node;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.LineBuilder;
+import javafx.scene.shape.HLineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.VLineTo;
 
 public class LabiPane extends Pane {
 
     private final LabiModel model;
+    private Path walls;
     
     public LabiPane(LabiModel model) {
         this.model = model;
@@ -21,22 +23,22 @@ public class LabiPane extends Pane {
     }
     
     private void init() {
-        LineBuilder<?> b = LineBuilder.create();
-        b.fill(Color.WHITE);
-        b.stroke(Color.WHITE);
+        walls = new Path();
+        walls.setFill(Color.WHITE);
+        walls.setStroke(Color.WHITE);
         for (int x = 0 ; x < model.getWidth(); x++) {
             for (int y = 0; y < model.getHeight(); y++) {
-                getChildren().add(b.startX(x).endX(x+1).startY(y).endY(y).build());
-                getChildren().add(b.startX(x).endX(x).startY(y).endY(y+1).build());
+                walls.getElements().add(new MoveTo(x, y));
+                walls.getElements().add(new HLineTo(x+1));
+                walls.getElements().add(new MoveTo(x, y));
+                walls.getElements().add(new VLineTo(y+1));
             }
         }
+        getChildren().add(walls);
         scaleXProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
-                for (Node n : getChildren()) {
-                    Line l = (Line) n;
-                    l.setStrokeWidth(1.0 / getScaleX());
-                }
+                walls.setStrokeWidth(1.0 / getScaleX());
             }
         });
     }
